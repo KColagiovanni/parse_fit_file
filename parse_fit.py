d7dbbeb9c637ef2d59e59d35ec4851247752a8f5
Kevin Colagiovanni <76464469+KColagiovanni@users.noreply.github.com>
2025-11-26 22:48:36 -0800

    Trying a different demo.

--- parse_fit.py
+++ parse_fit.py
@@ -1,8 +1,28 @@
-from garmin_fit_sdk import Decoder, Stream
+# from garmin_fit_sdk import Decoder, Stream
+#
+# stream = Stream.from_file('/home/kevin/Downloads/21091110845/21091110845_ACTIVITY.fit')
+# decoder = Decoder(stream)
+# messages, errors = decoder.read()
+#
+# print(errors)
+# print(messages)
+
+from garmin_fit_sdk import Decoder, Stream, Profile
 
 stream = Stream.from_file('/home/kevin/Downloads/21091110845/21091110845_ACTIVITY.fit')
-decoder = Decoder(stream)
-messages, errors = decoder.read()
 
-print(errors)
-print(messages)
+decoder = Decoder(stream)
+
+record_fields = set()
+def mesg_listener(mesg_num, message):
+    if mesg_num == Profile['mesg_num']['RECORD']:
+        for field in message:
+            record_fields.add(field)
+
+messages, errors = decoder.read(mesg_listener = mesg_listener)
+
+if len(errors) > 0:
+    print(f"Something went wrong decoding the file: {errors}")
+    # return
+
+print(record_fields)
